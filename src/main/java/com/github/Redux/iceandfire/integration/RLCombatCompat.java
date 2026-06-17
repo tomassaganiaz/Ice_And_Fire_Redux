3b86c11f4b554e75c8bc554693b71e825b72f8ae
organizacion_de_biblioteca <tomassaganiaz212@gmail.com>
2026-06-17 11:29:40 -0300

    Add Dragon Slayer enchant to RLCombat compat

--- src/main/java/com/github/Redux/iceandfire/integration/RLCombatCompat.java
+++ src/main/java/com/github/Redux/iceandfire/integration/RLCombatCompat.java
@@ -1,7 +1,10 @@
 package com.github.Redux.iceandfire.integration;
 
 import bettercombat.mod.event.RLCombatModifyDamageEvent;
+import com.github.Redux.iceandfire.CommonProxy;
+import com.github.Redux.iceandfire.entity.EntityDragonBase;
 import com.github.Redux.iceandfire.item.*;
+import net.minecraft.enchantment.EnchantmentHelper;
 import net.minecraft.entity.Entity;
 import net.minecraft.entity.EntityLivingBase;
 import net.minecraft.entity.player.EntityPlayer;
@@ -21,6 +24,14 @@ public class RLCombatCompat {
         Item item = event.getStack().getItem();
         if (item instanceof IHitEffect) {
             event.setDamageModifier(event.getDamageModifier() + ((IHitEffect) item).getHitEffectModifier((EntityLivingBase)target, player));
+        }
+
+        // Dragon Slayer enchantment
+        if (target instanceof EntityDragonBase) {
+            int slayerLevel = EnchantmentHelper.getEnchantmentLevel(CommonProxy.DRAGON_SLAYER, event.getStack());
+            if (slayerLevel > 0) {
+                event.setDamageModifier(event.getDamageModifier() * (1.0F + 0.05F + slayerLevel * 0.10F));
+            }
         }
     }
 }
